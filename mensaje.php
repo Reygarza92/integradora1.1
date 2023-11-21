7f50bc389c7c9d221f0d9c748d7b03b4c75b3c1b
Reynaldo Garza <93224007+Reygarza92@users.noreply.github.com>
2023-11-20 22:31:55 -0600

    Update mensaje.php

--- mensaje.php
+++ mensaje.php
@@ -3,14 +3,13 @@
   $name = htmlspecialchars($_POST['name']);
   $email = htmlspecialchars($_POST['email']);
   $phone = htmlspecialchars($_POST['phone']);
-  $website = htmlspecialchars($_POST['website']);
   $message = htmlspecialchars($_POST['message']);
 
   if(!empty($email) && !empty($message)){
     if(filter_var($email, FILTER_VALIDATE_EMAIL)){
       $receiver = "receiver_email_address"; //enter that email address where you want to receive all messages
       $subject = "From: $name <$email>";
-      $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\n\nMessage:\n$message\n\nRegards,\n$name";
+      $body = "Name: $name\nEmail: $email\nPhone: $phone\n\nMessage:\n$message\n\nRegards,\n$name";
       $sender = "From: $email";
       if(mail($receiver, $subject, $body, $sender)){
          echo "Your message has been sent";
